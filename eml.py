6f4390b7963e512f138b11c993bc0f211db56aea
Marco <rosa_frederic@yahoo.fr>
2024-10-10 00:53:10 +0200

    modification de TextEncoding pour améliorer la gestion des codes MIMES

--- eml.py
+++ eml.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-
-# import email
 from email import policy
 from email.parser import BytesParser
 import os
 from text_encoding import TextEncoding
 
+
 class Eml:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.message = Eml._load_eml(self, self.file_path)
-        self.name = Eml.get_name(self)
-        self.sender = Eml.get_sender(self)
-        self.to = Eml.get_to(self)
-        self.cc = Eml.get_cc(self)
-        self.bcc = Eml.get_bcc(self)
-        self.reply_to = Eml.get_reply_to(self)
-        self.return_path = Eml.get_return_path(self)
-        self.date = Eml.get_date(self)
-        self.message_id = Eml.get_message_id(self)
-        self.subject = Eml.get_subject(self)
-        # self.body = Eml.get_body()
+        self.message = self._load_eml(file_path)
+        self.name = self.get_name()
+        self.sender = self.get_sender()
+        self.to = self.get_to()
+        self.cc = self.get_cc()
+        self.bcc = self.get_bcc()
+        self.reply_to = self.get_reply_to()
+        self.return_path = self.get_return_path()
+        self.date = self.get_date()
+        self.message_id = self.get_message_id()
+        self.subject = self.get_subject()
 
     def _load_eml(self, file_path):
         with open(file_path, 'rb') as f:
@@ -35,14 +32,14 @@ class Eml:
         return TextEncoding.decode_header(self.message['From'])
 
     def get_to(self):
-        return TextEncoding.decode_header(self.message['To'])
-    
+        return self._get_addresses('To')
+
     def get_cc(self):
-        return TextEncoding.decode_header(self.message['Cc'])
-    
+        return self._get_addresses('Cc')
+
     def get_bcc(self):
-        return TextEncoding.decode_header(self.message['Bcc'])
-    
+        return self._get_addresses('Bcc')
+
     def get_reply_to(self):
         return TextEncoding.decode_header(self.message['Reply-To'])
 
@@ -54,23 +51,31 @@ class Eml:
 
     def get_message_id(self):
         return self.message['Message-ID']
-    
+
     def get_subject(self):
         return self.message['Subject']
 
     def get_body(self):
-        # Gérer les différents types de contenu (texte brut, HTML)
-        if self.message.is_multipart():
-            return ''.join(part.get_content() for part in self.message.iter_parts() if part.get_content_type() == 'text/plain')
-        else:
-            return self.message.get_body(preferencelist=('plain')).get_content()
+        try:
+            if self.message.is_multipart():
+                return ''.join(part.get_content() for part in self.message.iter_parts() if part.get_content_type() == 'text/plain')
+            else:
+                return self.message.get_body(preferencelist=('plain')).get_content()
+        except Exception as e:
+            print("Error retrieving body:", e)
+            return None
+
+    def _get_addresses(self, field_name):
+        """
+        Helper method to parse multiple email addresses in the To, Cc, and Bcc fields.
+        """
+        addresses = self.message[field_name]
+        if addresses:
+            return [TextEncoding.decode_header(addr) for addr in addresses.split(',')]
+        return []
 
     def save(self, new_file_path=None):
         if new_file_path is None:
             new_file_path = self.file_path
         with open(new_file_path, 'wb') as f:
             f.write(self.message.as_bytes())
-
-
-
-   
