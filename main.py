d51ded4c7c6b0505e910cd5222f05ca0ae15efa0
Marco <rosa_frederic@yahoo.fr>
2024-10-12 08:14:45 +0200

    simplification des appels

--- main.py
+++ main.py
@@ -7,17 +7,7 @@ file_path="depot_eml/test YAHOO PJ nom avec caractères spéciaux.eml"
 
 def main(file_path):
     eml = Eml(file_path)
-    print("Name:", eml.name)
-    print("Sender:", eml.sender)
-    print("To:", eml.to)
-    print("Cc:", eml.cc)
-    print("Bcc:", eml.bcc)
-    print("Reply-To:", eml.reply_to)
-    print("Return-Path:", eml.return_path)
-    print("Date:", eml.date)
-    print("Message-ID:", eml.message_id)
-    print("Subject:", eml.subject)
-    # print("Body:", eml.get_body())
+    Eml.print_eml(eml)
 
 if __name__ == "__main__":
     file_path = "depot_eml/test YAHOO PJ nom avec caractères spéciaux.eml"
