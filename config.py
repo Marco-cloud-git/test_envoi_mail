d125179d257a673a83017827850ff948c8d26238
Marco <rosa_frederic@yahoo.fr>
2024-10-14 22:28:27 +0200

    ajout de la modification des expediteurs, reply-to et retur-path

---
+++ config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+entry_folder_path = "entree_eml"
+destination_folder_path = "sortie_eml"
--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from eml import Eml
+from eml import Eml, ModifyEml
+import utils
+import os
+import config
+
+eml_name = "test YAHOO PJ nom avec caractères spéciaux.eml"
+eml_file_path = os.path.join(config.entry_folder_path, eml_name)
 
 
-file_path="depot_eml/test YAHOO PJ nom avec caractères spéciaux.eml"
+new_sender = "new_sender@test.fr"
+new_reply_to = None         #"new_reply_to@test.fr"
+new_return_path = "new_return_path@test.fr"
 
-def main(file_path):
-    eml = Eml(file_path)
+
+def main(eml_file_pat):
+    # Vérifier que le répertoire de sortie existe
+    utils.directory_exist(config.entry_folder_path)
+    utils.directory_exist(config.destination_folder_path)
+
+    # Vérifier que le fichier existe
+    utils.file_exist(eml_file_path)
+
+    # Charger l'email d'entrée
+    eml = Eml(eml_file_pat)
     Eml.print_eml(eml)
+    name = eml.get_name()
+
+    # Créer un modificateur pour l'objet Eml
+    eml_modifier = ModifyEml(eml)
+
+    # Modifier l'expéditeur, Return_Path et Reply_To
+    eml_modifier.set_sender(
+        sender=new_sender, reply_to=new_reply_to, return_path=new_return_path
+    )
+
+    # Construire le chemin de destination et sauvegarder
+    destination_file_path = os.path.join(config.destination_folder_path, name)
+    print(f"Destination : {destination_file_path}")
+
+    # Sauvegarder l'email modifié
+    eml_modifier.save(destination_file_path)
+
 
 if __name__ == "__main__":
-    file_path = "depot_eml/test YAHOO PJ nom avec caractères spéciaux.eml"
+    file_path = "entree_eml/test YAHOO PJ nom avec caractères spéciaux.eml"
     main(file_path)
