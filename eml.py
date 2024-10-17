28a1e612515878d29c9574e63e3e9d2997cd3831
Marco <rosa_frederic@yahoo.fr>
2024-10-17 11:36:58 +0200

    deplacement de la classe TextEncoding

--- eml.py
+++ eml.py
@@ -4,10 +4,10 @@ from email import policy
 from email.parser import BytesParser
 from email.utils import format_datetime, make_msgid
 import os
-from text_encoding import TextEncoding
 from datetime import datetime
 import pytz
 from functools import wraps
+from email.header import decode_header
 
 
 class Eml:
@@ -350,3 +350,30 @@ class ModifyEml:
             raise AttributeError("L'objet EML ne possède pas de méthode 'as_bytes'.")
         with open(new_file_path, "wb") as f:
             f.write(self.eml_object.as_bytes())
+
+
+class TextEncoding:
+    @staticmethod
+    def decode_header(encoded_string):
+        # Vérifier si la chaîne est None avant le décodage
+        if encoded_string is None:
+            return ""  # Retourne une chaîne vide si l'en-tête n'existe pas
+        decoded_fragments = []
+        for part, encoding in decode_header(encoded_string):
+            if isinstance(part, bytes):
+                decoded_fragments.append(part.decode(encoding or "utf-8"))
+            else:
+                decoded_fragments.append(part)
+        return "".join(decoded_fragments)
+
+
+class InvalidEncodingError(Exception):
+    """A custom exception class to report Invalid Encoding errors."""
+
+    def __init__(self, encoded_value: str = ""):
+        self.encoded_value = encoded_value
+        self.message = "Invalid encoding used"
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.message}. Encoded string causing the error: {self.encoded_value}"
